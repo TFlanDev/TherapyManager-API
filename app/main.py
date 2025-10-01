a1962c677affb7d123874fe407e4fdd78c73ddae
TFlanDev <Tdflan90@gmail.com>
2025-09-30 19:01:41 -0700

    chore: Fix formatting

--- app/main.py
+++ app/main.py
@@ -4,19 +4,11 @@ from . import models, schemas, crud
 from .database import SessionLocal, engine, get_db
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
-@app.get("/")
-async def root():
-    return {"message" : "Hello World"}
-
-@app.get("/hello/{name}")
-async def say_hello(name : str):
-    return {"message" : f"Hello {name}"}
 
 @app.post("/therapists/", response_model=schemas.TherapistCreate)
 def create_therapist(therapist: schemas.TherapistCreate, db: Session = Depends(get_db)):
     # check if therapist already exists in db
     return crud.create_therapist(db=db, therapist=therapist)
-
 
 @app.get("/therapist/{therapist_id}", response_model=schemas.TherapistGet)
 def get_therapist(therapist_id : int, db : Session = Depends(get_db)):
