f081268b8678313a9d7ec58aea99ee9118fe70c1
TFlanDev <Tdflan90@gmail.com>
2025-09-30 18:47:34 -0700

    feat: add endpoints to post and get therapist records

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import SessionLocal, engine, get_db
 app = FastAPI()
-
+models.Base.metadata.create_all(bind=engine)
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
@@ -9,3 +11,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name : str):
     return {"message" : f"Hello {name}"}
+
+@app.post("/therapists/", response_model=schemas.TherapistCreate)
+def create_therapist(therapist: schemas.TherapistCreate, db: Session = Depends(get_db)):
+    # check if therapist already exists in db
+    return crud.create_therapist(db=db, therapist=therapist)
+
+
+@app.get("/therapist/{therapist_id}", response_model=schemas.TherapistGet)
+def get_therapist(therapist_id : int, db : Session = Depends(get_db)):
+    return crud.get_therapist(db, therapist_id=therapist_id)
