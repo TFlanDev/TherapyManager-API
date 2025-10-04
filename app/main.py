2f379d5f340bbf28e2ffff4953c7b07f9c975b02
TFlanDev <Tdflan90@gmail.com>
2025-10-04 12:19:36 -0700

    refactor(api): Standardize enpoint names

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .database import engine, get_db
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-@app.post("/create/therapist/", response_model=schemas.TherapistCreate)
+@app.post("/therapist/", response_model=schemas.TherapistCreate)
 def create_therapist(therapist: schemas.TherapistCreate, db: Session = Depends(get_db)):
     return crud.create_therapist(db=db, therapist=therapist)
 
@@ -19,7 +19,7 @@ def get_all_therapists(include : Optional[str] = None, db : Session = Depends(ge
     return crud.get_all_therapists(db=db, include=include)
 
 
-@app.post("/create/patient/", response_model=schemas.PatientCreate)
+@app.post("/patient/", response_model=schemas.PatientCreate)
 def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
     return crud.create_patient(db=db, patient=patient)
 
@@ -27,6 +27,6 @@ def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)
 def get_patient(patient_id : int, db : Session = Depends(get_db)):
     return crud.get_patient(db=db, patient_id=patient_id)
 
-@app.put("/assignpatient/{therapist_id}/{patient_id}", response_model=schemas.PatientUpdate)
+@app.put("/patient/{patient_id}/", response_model=schemas.PatientGet)
 def update_patient(patient_id : int, therapist_id : int, db : Session = Depends(get_db)):
     return crud.update_patient(db=db, patient_id=patient_id, therapist_id=therapist_id)
