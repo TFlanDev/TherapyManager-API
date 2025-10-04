748730ac9b8519ca8ec06e3ca7f38574c52e82a1
TFlanDev <Tdflan90@gmail.com>
2025-10-04 11:40:38 -0700

    feat: Conditionally include patient data when getting individual therapists

--- app/crud.py
+++ app/crud.py
@@ -2,13 +2,15 @@ from typing import Optional
 from sqlalchemy.orm import Session
 from . import database, models, schemas
 
-def get_therapist(db : Session, therapist_id : int):
-    return db.query(models.Therapist).filter(models.Therapist.id == therapist_id).first()
+def get_therapist(db : Session, therapist_id : int, include : Optional[str] = None):
+    if include and 'patients' in include:
+        return db.query(models.Therapist).filter(models.Therapist.id == therapist_id).first()
+    return db.query(models.Therapist.id, models.Therapist.name).filter(models.Therapist.id == therapist_id).first()
 
 def get_all_therapists(db : Session, include : Optional[str] = None):
     if include and 'patients' in include:
         return db.query(models.Therapist).all()
-    return db.query(models.Therapist.id, models.Therapist.name)
+    return db.query(models.Therapist.id, models.Therapist.name).all()
 
 
 def create_therapist(db : Session, therapist: schemas.TherapistCreate):
@@ -30,7 +32,7 @@ def get_patient(db : Session, patient_id : int):
     return db.query(models.Patient).filter(models.Patient.id == patient_id).first()
 
 def update_patient(db : Session, patient_id : int, therapist_id : int):
-    patient_to_update = db.query(models.Patient).filter(patient_id == patient_id).first()
+    patient_to_update = db.query(models.Patient).filter(models.Patient.id == patient_id).first()
     patient_to_update.therapist_id = therapist_id
     db.commit()
     db.refresh(patient_to_update)
